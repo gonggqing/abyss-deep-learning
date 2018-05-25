810efc4a661d77533d13e952117821a2023dbe3c
steve p <s.potiris@acfr.usyd.edu.au>
2018-05-25 17:25:31 +1000

    Added score threshold to metrics

--- abyss_deep_learning/metrics.py
+++ abyss_deep_learning/metrics.py
@@ -24,7 +24,7 @@ def result_to_series(result):
         })
     return pd.DataFrame(frame, columns=frame[0].keys())
 
-def calculate_tfpn(predicted, object_gts, iou_thresh=0.5, matching='one-to-one'):
+def calculate_tfpn(predicted, object_gts, matching='one-to-one', iou_thresh=0.5, scores=None, score_thresh=None):
     '''Calculate True Positive, False Positive and False Negatives given two objects of the same class
         inputs are pandas.DataFrame as returned by result_to_series
     '''
@@ -37,24 +37,27 @@ def calculate_tfpn(predicted, object_gts, iou_thresh=0.5, matching='one-to-one')
         'one-to-many': lambda _, __: raise_(NotImplementedError("one-to-many not yet implemented")),
         'many-to-one': matching_many_to_one,
         'many-to-many': matching_many_to_many
-    }[matching](ious, iou_thresh)
+    }[matching](ious, iou_thresh, scores=scores, score_thresh=score_thresh)
     return TP, FP, FN
 
-def calc_image_stats(predicted, object_gts, matching, iou_thresh=0.5):
+def calc_image_stats(predicted, object_gts, matching, iou_thresh=0.5, score_thresh=None):
     image_stats = {}
     for class_id in np.unique(predicted['class_id']):
+        class_mask_pred = predicted['class_id'] == class_id
         (TP, FP, FN) = calculate_tfpn(
-            predicted[predicted['class_id'] == class_id],
+            predicted[class_mask_pred],
             object_gts[object_gts['class_id'] == class_id],
-            iou_thresh=iou_thresh, matching=matching)
-        image_stats[class_id] = {
+            iou_thresh=iou_thresh, matching=matching,
+            scores=predicted[class_mask_pred]['score'],
+            score_thresh=score_thresh)
+        image_stats[str(class_id)] = {
             'TP': TP,
             'FP': FP,
             'FN': FN,
         }
     return image_stats
 
-def matching_one_to_one(ious, iou_thresh):
+def matching_one_to_one(ious, iou_thresh, scores=None, score_thresh=None):
     '''Definitions (one-to-one):
         TP = number of detections with (IoU > 0.5)
         FP = number of detections with (IoU <= 0.5 or detected more than once)
@@ -63,29 +66,36 @@ def matching_one_to_one(ious, iou_thresh):
     TP = np.zeros(ious.shape[0], dtype=np.bool)
     FP = np.zeros(ious.shape[0], dtype=np.bool)
     FN = np.zeros(ious.shape[1], dtype=np.bool)
-
-    ious[ious < iou_thresh] = 0
+    invalid = ious <= iou_thresh
+    if scores is not None:
+        invalid[scores <= score_thresh, :] = True
+    ious[invalid] = 0
     pred_idx_ptr, gt_idx = linear_sum_assignment(1 - ious)
     pred_idx = np.arange(len(TP))[pred_idx_ptr]
     match_costs = ious[pred_idx, gt_idx]
-    sufficient_area = match_costs > iou_thresh
-    pred_idx = pred_idx[sufficient_area]
-    gt_idx = gt_idx[sufficient_area]
+    valid = match_costs > iou_thresh
+    if scores is not None:
+        valid &= scores[pred_idx] > score_thresh
+    pred_idx = pred_idx[valid]
+    gt_idx = gt_idx[valid]
     # pred_idx_unmatched = np.array(list(set(range(ious.shape[0])) - set(pred_idx.tolist())))
     gt_idx_unmatched = np.array(list(set(range(ious.shape[1])) - set(gt_idx.tolist())))
 
     TP[pred_idx] = True
-    FP = ~TP
+    FP |= ~TP
     if gt_idx_unmatched.size:
         FN[gt_idx_unmatched] = True
     return TP, FP, FN
 
-def matching_many_to_one(ious, iou_thresh):
+
+def matching_many_to_one(ious, iou_thresh, scores=None, score_thresh=None):
     '''Definitions (many-to-one):
         TP = number of detections with IoU > iou_thresh
         FP = number of detections with IoU <= iou_thresh
         FN = number of objects with IoU <= iou_thresh'''
     ious[ious < iou_thresh] = 0
+    if scores is not None:
+        ious[scores <= score_thresh, :] = 0
     FP = np.sum(ious, axis=1) < 1e-6
     TP = ~FP
     FN = np.zeros(ious.shape[1], dtype=np.bool)
@@ -98,16 +108,18 @@ def matching_many_to_one(ious, iou_thresh):
         FN[gt_idx_unmatched] = True
     return TP, FP, FN
 
-def matching_many_to_many(ious, iou_thresh):
+def matching_many_to_many(ious, iou_thresh, scores=None, score_thresh=None):
     '''Definitions (many-to-many):
         TP = number of detections with IoU > iou_thresh
         FP = number of detections with IoU <= iou_thresh
         FN = number of objects with IoU <= iou_thresh'''
     ious[ious < iou_thresh] = 0
+    if scores is not None:
+        ious[scores <= score_thresh, :] = 0
     FP = np.zeros(ious.shape[0], dtype=np.bool)
     TP = np.zeros(ious.shape[0], dtype=np.bool)
     FN = np.ones(ious.shape[1], dtype=np.bool)
-    conditions = ious > iou_thresh
+    conditions = ious > 0
     pred_idx, gt_idx = np.nonzero(conditions)
     TP[np.unique(pred_idx)] = True
     FN[np.unique(gt_idx)] = False
