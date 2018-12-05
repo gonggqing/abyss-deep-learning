aee266bddf047639a83601ed92d9e7f6302688fb
Kent Hu <khu.1998@icloud.com>
2018-12-05 14:41:56 +1100

    Initialized branch for coco conversion

--- examples/create_dataset_structure_from_jsons.py
+++ examples/create_dataset_structure_from_jsons.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
 
-import numpy as np
-import json
-from pycocotools.coco import COCO
-import os
 import argparse
 import glob
+import json
+import logging
+import os
+import re
+from inspect import currentframe
+
 import cv2
+from pycocotools.coco import COCO
+
+
+def debug_print(arg):
+    """Debugging tool to print line number followed by normal print statement arguments
+
+    """
+    frame_info = currentframe()
+    print("Line", frame_info.f_back.f_lineno, ":", arg)
 
 
 def get_unique_list_of_videos(coco):
@@ -20,10 +31,13 @@ def get_unique_list_of_videos(coco):
 
     """
     video_list = []
-    imgIds = coco.getImgIds(imgIds=[]) # Load all images
+    imgIds = coco.getImgIds(imgIds=[])  # Load all images
     for id in imgIds:
         video_path = coco.loadImgs(id)[0]["path"]
-        video_name = os.path.basename(video_path).split(':')[0].strip(" (1)")
+        #debug_print("video path is {}".format(video_path))
+        video_name = os.path.basename(video_path).split(':')[0]
+        video_name = re.sub(r" \([0-9]\)", "", video_name)
+        #debug_print(video_name)
         video_list.append(video_name)
     return list(set(video_list))
 
@@ -67,11 +81,12 @@ def get_video_path(coco):
         str: The video path
 
     """
-    imgIds = coco.getImgIds(imgIds=[]) # Load all images
+    imgIds = coco.getImgIds(imgIds=[])  # Load all images
     id = imgIds[0]
     video_path = coco.loadImgs(id)[0]["path"].split(":")[0]
-    print(video_path)
+    debug_print(video_path)
     return video_path
+
 
 def get_video_at_default_path(coco, default_path):
     """Gets the video at the provided video default path. Full path is default_path/basename.
@@ -99,6 +114,8 @@ def get_args():
 
 
 def main(args):
+    logging.basicConfig(filename='error.log', filemode='w', level=logging.DEBUG)
+
     for f in glob.glob(os.path.join(args.cf_json_dir, "**", "*.json"), recursive=True):
         coco = COCO(annotation_file=f)
         if not check_number_of_videos_per_json(coco):
@@ -107,10 +124,14 @@ def main(args):
         # Make a new folder for the video, and frames
         if not os.path.exists(os.path.join(args.output_dir, video_basename, "frames")):
             os.makedirs(os.path.join(args.output_dir, video_basename, "frames"))
+        else:
+            continue
         frames_path = os.path.join(args.output_dir, video_basename, "frames")
 
         # Open the video
-        cap = cv2.VideoCapture(get_video_at_default_path(coco, args.default_video_path))
+
+        video_path = get_video_at_default_path(coco, args.default_video_path)
+        cap = cv2.VideoCapture(video_path)
 
         # Save all the images to the frames folder
         for im_idx in range(len(coco.dataset['images'])):
@@ -118,18 +139,22 @@ def main(args):
             img_path = coco.dataset['images'][im_idx]["path"]
             # The image frame number
             img_frame_number = int(img_path.split(":")[-1])
-            print(img_frame_number)
+            #print(img_frame_number)
             # New Path
             new_path = os.path.join(frames_path, video_basename + "_" + str(img_frame_number) + ".png")
             # Set the frame in the video
             cap.set(1, img_frame_number)
             # Read the Image
             ret, img = cap.read()
-            if ret == True:
+            if ret:
                 # Write the image to the new path
                 cv2.imwrite(new_path, img)
             else:
-                print("FAILED TO GET FRAME")
+                debug_print("FAILED TO GET FRAME")
+                logging.debug("Failed to get frame {} from {}".format(img_frame_number, video_path))
+                #debug_print(f)
+                #debug_print(get_unique_list_of_videos(coco))
+                #return
             # New Coco Dataset
             coco.dataset['images'][im_idx]["path"] = new_path
         # Write the new dataset
@@ -139,6 +164,5 @@ def main(args):
 
 if __name__ == "__main__":
     main(get_args())
-
 
     # print(video_list)
