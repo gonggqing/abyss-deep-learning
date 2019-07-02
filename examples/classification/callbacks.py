5082fbceb68f2b9574a8b2c00b9619b046afeef5
Jackson Shields <jacksonhshields@gmail.com>
2019-07-02 17:48:17 +1000

    removed old training scripts. Updated training script to work with TRAINS

--- examples/classification/callbacks.py
+++ examples/classification/callbacks.py
@@ -155,46 +155,44 @@ class TrainValTensorBoard(TensorBoard):
 
 
 
+class TrainsCallback(keras.callbacks.Callback):
+    """
+    Publishes scalar logs to the Trains server
+    """
+    def __init__(self, logger):
+        """
+        Initialises the callback
+        Args:
+            logger: (Task Logger). The logger to report scalars to. Initialise with task.get_logger() where task is the trains.Task class.
+        """
+        super(TrainsCallback, self).__init__()
+        self.logger = logger
+        self.epoch_count = 0
 
-from keras.callbacks import TensorBoard
-from tensorboard.plugins.pr_curve import summary as pr_summary
+    def on_batch_end(self, batch, logs=None):
+        """
+        Publishes batchwise information
 
-# Check complete example in:
-# https://github.com/akionakamura/pr-tensorboard-keras-example
-# class PRTensorBoard(TensorBoard):
-#     def __init__(self, *args, **kwargs):
-#         # One extra argument to indicate whether or not to use the PR curve summary.
-#         self.pr_curve = kwargs.pop('pr_curve', True)
-#         super(PRTensorBoard, self).__init__(*args, **kwargs)
-#
-#         global tf
-#         import tensorflow as tf
-#
-#     def set_model(self, model):
-#         super(PRTensorBoard, self).set_model(model)
-#
-#         if self.pr_curve:
-#             # Get the prediction and label tensor placeholders.
-#             predictions = self.model._feed_outputs[0]
-#             labels = tf.cast(self.model._feed_targets[0], tf.bool)
-#             # Create the PR summary OP.
-#             self.pr_summary = pr_summary.op(tag='pr_curve',
-#                                             predictions=predictions,
-#                                             labels=labels,
-#                                             display_name='Precision-Recall Curve')
-#
-#     def on_epoch_end(self, epoch, logs=None):
-#         super(PRTensorBoard, self).on_epoch_end(epoch, logs)
-#
-#         if self.pr_curve and self.validation_data:
-#             # Get the tensors again.
-#             tensors = self.model._feed_targets + self.model._feed_outputs
-#             # Predict the output.
-#             predictions = self.model.predict(self.validation_data[:-2])
-#             # Build the dictionary mapping the tensor to the data.
-#             val_data = [self.validation_data[-2], predictions]
-#             feed_dict = dict(zip(tensors, val_data))
-#             # Run and add summary.
-#             result = self.sess.run([self.pr_summary], feed_dict=feed_dict)
-#             self.writer.add_summary(result[0], epoch)
-#         self.writer.flush()
+        Args:
+            batch: (int) the current batch
+            logs: (dict) the logs
+
+        Returns:
+            None
+        """
+        for k,v in logs.items():
+            self.logger.report_scalar("batch_" + k, "series A", iteration=self.epoch_count+batch, value=v)
+
+    def on_epoch_end(self, epoch, logs=None):
+        """
+        Publishes information at the end of each epoch
+        Args:
+            epoch: (int) the epoch
+            logs: (dict) the logs
+
+        Returns:
+            None
+        """
+        for k,v in logs.items():
+            self.logger.report_scalar(k, "series A", iteration=epoch, value=v)
+        self.epoch_count += 1
