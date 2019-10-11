731af240eed2d35e601fd2e97cdf43db55a11d51
Steven Potiris <spotiris@abysssolutions.com.au>
2019-10-11 17:14:44 +1100

    Clean up train_segmenter.py; tested training continuation;

--- examples/segmentation/train_segmenter.py
+++ examples/segmentation/train_segmenter.py
@@ -14,7 +14,7 @@ import logging
 import argparse
 
 import abyss_deep_learning.imgaug as abyss_imgaug
-import abyss_deep_learning.keras.zoo.segmentation.deeplab_v3_plus.util as deeplab
+import abyss_deep_learning.keras.zoo.segmentation.deeplab_v3_plus.util as deeplab_util
 from abyss_deep_learning.keras.utils import lambda_gen
 from abyss_deep_learning.datasets.coco import ImageSemanticSegmentationDataset
 
@@ -22,11 +22,58 @@ import tensorflow as tf
 from imgaug.augmentables.segmaps import SegmentationMapsOnImage
 
 DEFAULT_CONFIG = {
-    "batch_size": 4,
-    "epochs": 5,
-    "learning_rate": 1e-4,
-    "new_heads": True,
+    'batch_size': 4,
+    'epochs': 5,
+    'learning_rate': 1e-4,
+    'new_heads': True,
 }
+
+
+def get_args(cmd_line=None):
+    """Construct arguments from command line.
+
+    Returns:
+        argparse.namespace: The arguments for this application
+
+    Args:
+        cmd_line (str, optional): Command line string to use instead of
+            reading from sys.argv.
+    """
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description="""
+        Train a segmentation network.
+        Usage:
+
+        # Train a model, initializing new heads (defined in config.json:/model).
+            train_segmenter.py coco.train.json config.json --dataset-val coco.val.json
+
+        # Continue training a model (given the same config.json, and model.h5)
+            train_segmenter.py coco.train_set_2.json config.json \\
+            --dataset-val coco.val.json \\
+            --load model.h5""")
+    parser.add_argument(
+        'config',
+        help="Specifies the path to the config JSON for the model.")
+    parser.add_argument('dataset_train', type=str,
+                        help="Path to the coco dataset")
+    parser.add_argument("--dataset-val", type=str,
+                        help="Path to the validation coco dataset")
+    parser.add_argument(
+        "--load", type=str,
+        help=(
+            "Path to model weights to load."
+            "Also accepts 'pascal_voc' (21 classes) or 'cityscapes' (19)."))
+    parser.add_argument(
+        "--output-config",
+        help="TODO (update): Outputs the default config and exits.",
+        action='store_true')
+    parser.add_argument(
+        '--verbose', '-v',
+        action='store_const', const=logging.INFO,
+        dest='loglevel', help="verbose output to stderr")
+    args = parser.parse_args(cmd_line)
+    return args
 
 
 class AnnotationClassMapper:
@@ -44,48 +91,8 @@ class AnnotationClassMapper:
 
     def translate(self, annotation):
         output = dict(annotation)
-        output["category_id"] = self.category_to_class_map[annotation['category_id']]
+        output['category_id'] = self.category_to_class_map[annotation['category_id']]
         return output
-
-
-def get_args(cmd_line=None):
-    """Construct arguments from command line.
-
-    Returns:
-        argparse.namespace: The arguments for this application
-
-    Args:
-        cmd_line (str, optional): Command line string to use instead of
-            reading from sys.argv.
-    """
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawTextHelpFormatter,
-        description="""
-        Train a segmentation network.
-        TODO: usage
-        """)
-    parser.add_argument(
-        "config",
-        help="Specifies the path to the config JSON for the model.")
-    parser.add_argument("dataset_train", type=str,
-                        help="Path to the coco dataset")
-    parser.add_argument("--dataset-val", type=str,
-                        help="Path to the validation coco dataset")
-    parser.add_argument(
-        "--weights", type=str,
-        help=(
-            "Path to model weights to load."
-            "Also accepts 'pascal_voc' (21 classes) or 'cityscapes' (19)."))
-    parser.add_argument(
-        "--output-config",
-        help="TODO (update): Outputs the default config and exits.",
-        action='store_true')
-    parser.add_argument(
-        '--verbose', '-v',
-        action='store_const', const=logging.INFO,
-        dest='loglevel', help="verbose output to stderr")
-    args = parser.parse_args(cmd_line)
-    return args
 
 
 def get_class_mapping(dataset, categories_config):
@@ -110,7 +117,7 @@ def get_class_mapping(dataset, categories_config):
         category_map[pair['name']]: pair['id']
         for pair in categories_config
     }
-    logging.info("category_to_class_map")
+    logging.info('category_to_class_map')
     logging.info(category_to_class_map)
     return AnnotationClassMapper(category_to_class_map)
 
@@ -152,14 +159,14 @@ def setup_dataset(coco_path, model_config, process_config, categories_config):
         return image2.astype('float32') / 127.5 - 1, targets2
 
     augmentation = abyss_imgaug.sequential_from_dicts(
-        process_config["augmentation"])
+        process_config['augmentation'])
     logging.info("Using augmentation:")
     logging.info([i for i in augmentation])
     output_shapes = (
         model_config['input_shape'],
         model_config['input_shape'][:-1] + (model_config['classes'],))
     dataset = ImageSemanticSegmentationDataset(
-        coco_path, num_classes=model_config["classes"])
+        coco_path, num_classes=model_config['classes'])
     translator = get_class_mapping(dataset, categories_config)
     dataset.translator = translator
 
@@ -168,8 +175,52 @@ def setup_dataset(coco_path, model_config, process_config, categories_config):
     generator = tf.data.Dataset.from_generator(
         hack_dataset(generator),
         output_types=(tf.float32, tf.float32),
-        output_shapes=output_shapes).batch(process_config["batch_size"])
+        output_shapes=output_shapes)\
+        .prefetch(2 * process_config['batch_size'])\
+        .batch(process_config['batch_size'])
     return dataset, generator
+
+
+def load_config(config_path):
+    """Load the default configuration, and update it with the config file.
+
+    Args:
+        config_path (str): Path to the config.json
+
+    Returns:
+        dict: The configuration dict.
+
+    Raises:
+        ValueError: Bad model type.
+        module: The model_util module associated with the config.
+    """
+    with open(config_path, 'r') as file:
+        user_config = json.load(file)
+
+    # Setup model utilities
+    if user_config['model']['type'] == 'deeplab':
+        model_util = deeplab_util
+    else:
+        raise ValueError(f"Unknown model type `{user_config['model']['type']}`")
+    del user_config['model']['type']  # TODO: Review if this is good, or should be CLI
+
+    # Update configs
+    config = {
+        'model': dict(model_util.DEFAULT_CONFIG),
+        'training': dict(DEFAULT_CONFIG),
+        'validation': dict(DEFAULT_CONFIG),
+        'categories': user_config['categories'] or dict()
+    }
+
+    config['model'].update(user_config['model'])
+    config['training'].update(user_config['training'])
+    config['validation'].update(user_config['validation'])
+    config['model']['input_shape'] = tuple(user_config['model']['input_shape'])
+    del user_config
+
+    logging.info("Updated config:")
+    logging.info(config)
+    return config, model_util
 
 
 def main(args):
@@ -188,61 +239,51 @@ def main(args):
     logging.info('--verbose enabled')
     logging.info(args)
 
-    # Load configs
-    model_config = dict(deeplab.DEFAULT_CONFIG)
-    training_config = dict(DEFAULT_CONFIG)
-    validation_config = dict(DEFAULT_CONFIG)
+    # Load config
+    config, model_util = load_config(args.config)
     if args.output_config:
-        print(json.dumps(model_config))
+        print(json.dumps(config))
         return 0
-    with open(args.config, "r") as file:
-        config = json.load(file)
-        model_config.update(config['model'])
-        training_config.update(config['training'])
-        validation_config.update(config['validation'])
-        categories_config = config['categories']
-    model_config["input_shape"] = tuple(model_config["input_shape"])
-    logging.info("Model config:")
-    logging.info(model_config)
-    logging.info("Training config:")
-    logging.info(training_config)
-    logging.info("Validation config:")
-    logging.info(validation_config)
 
     # Set up dataset
     dataset_train, gen_train = setup_dataset(
-        args.dataset_train, model_config, training_config, categories_config)
-    validation_steps, gen_val = validation_config['steps'], None
+        args.dataset_train, config['model'], config['training'], config['categories'])
+    validation_steps, gen_val = config['validation']['steps'], None
+
     if args.dataset_val:
         logging.info("Using validation.")
         dataset_val, gen_val = setup_dataset(
-            args.dataset_val, model_config, validation_config, categories_config)
+            args.dataset_val, config['model'], config['validation'], config['categories'])
         if validation_steps is None:
             validation_steps = len(
-                dataset_val.data_ids) // validation_config["batch_size"]
+                dataset_val.data_ids) // config['validation']['batch_size']
     steps_per_epoch = len(
-        dataset_train.data_ids) // training_config["batch_size"]
+        dataset_train.data_ids) // config['training']['batch_size']
 
     # # Set batch norm policy
     # for layer in model.layers:
-    #     if type(layer).__name__.startswith("BatchNormalization"):
-    #         layer.trainable = layer.trainable and training_config['train_batch_norm']
+    #     if type(layer).__name__.startswith('BatchNormalization'):
+    #         layer.trainable = layer.trainable and config['training']['train_batch_norm']
 
     # Set up training
-    if training_config["new_heads"]:
+    if config['training']['new_heads']:
         logging.info("Creating new head.")
-        model = deeplab.make_model(**model_config)
+        model = model_util.make_model(**config['model'])
     else:
         logging.info("Loading model.")
-        model = tf.keras.models.load_model(args.weights)
+        model = tf.keras.models.load_model(args.load)
     callbacks = [
+        tf.keras.callbacks.TensorBoard(".", write_graph=False),
         tf.keras.callbacks.ModelCheckpoint(
-            filepath='model.{epoch:03d}-{val_loss:.3f}.h5',
+            filepath='model_{epoch:03d}_{val_loss:.3f}.h5',
             save_best_only=True, save_weights_only=False, verbose=1)
     ]
 
+    with open("model_def.json", 'w') as file:
+        file.write(model.to_json())
+
     # Start training
-    optimizer = tf.keras.optimizers.Nadam(lr=training_config["learning_rate"])
+    optimizer = tf.keras.optimizers.Adam(lr=config['training']['learning_rate'])
     model.compile(
         optimizer=optimizer,
         loss='categorical_crossentropy',
@@ -253,11 +294,11 @@ def main(args):
         steps_per_epoch=steps_per_epoch,
         validation_data=gen_val,
         validation_steps=validation_steps,
-        epochs=training_config["epochs"],
+        epochs=config['training']['epochs'],
         callbacks=callbacks,
         verbose=1)
     return 0
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     exit(main(get_args()))
