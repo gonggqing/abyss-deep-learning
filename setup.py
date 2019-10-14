2c5befa05744160eff7f23050ee5c2a0ed2931f8
Steven Potiris <spotiris@abysssolutions.com.au>
2019-10-14 16:14:19 +1100

    examples/segmentation/train_segmenter.py > abyss-segmentation-train Added segmentation default config.

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ from distutils.core import setup
 setup(
     name='Abyss Deep Learning',
     version="1.0.0",
-    description='Scripts and utilities for deep learning. Contains most coco'
-    'scripts and extra utilies for pre-processing',
+    description="""
+        Scripts and utilities for deep learning. Contains most coco scripts and extra
+        utilities for pre-processing.""",
     author='Abyss Solutions',
     author_email='tech@abysssolutions.com.au',
     url='http://github.com/abyss-solutions/deep-learning',
@@ -16,24 +17,21 @@ setup(
         'abyss_deep_learning.datasets',
         'abyss_deep_learning.datasets.mrcnn',
     ],
-    package_data={
-        # 'abyss_deep_learning': ["third-party"]
-    },
     scripts=[
+        "applications/abyss-segmentation-train",
         "applications/argparse.bash",
         "applications/coco-calc",
         "applications/coco-check-data-pollution",
         "applications/coco-draw",
         "applications/coco-extract-masks",
         "applications/coco-from-csv",
-        "applications/coco-from-segmentation",
         "applications/coco-from-images",
+        "applications/coco-from-segmentation",
         "applications/coco-from-video",
         "applications/coco-images",
         "applications/coco-merge",
         "applications/coco-metrics",
         "applications/coco-overlay-masks",
-        "applications/coco-sample",
         "applications/coco-sample",
         "applications/coco-select",
         "applications/coco-split",
@@ -55,4 +53,8 @@ setup(
         "applications/retinanet-predict",
         "applications/tf-segmentation-predict",
         ],
+    data_files=[
+        ('etc/abyss/ml/segmentation/training',
+            ['etc/abyss/ml/segmentation/training/config.json']),
+    ],
 )
